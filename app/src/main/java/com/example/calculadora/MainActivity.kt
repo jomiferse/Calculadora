c6b7d254c933d1c1daf316fe7429e6a0d36a1ae8
jomiferse <ljose500.jf@gmail.com>
2019-09-26 17:44:48 +0200

    Búscador web en landscape

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.calculadora
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +10,7 @@ import android.widget.Toast
 import java.math.RoundingMode
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.UnsupportedOperationException
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
 import kotlin.math.roundToLong
@@ -17,88 +21,110 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btUno.setOnClickListener { funcionExpresion("1", true) }
-        btDos.setOnClickListener { funcionExpresion("2", true) }
-        btTres.setOnClickListener { funcionExpresion("3", true) }
-        btCuatro.setOnClickListener { funcionExpresion("4", true) }
-        btCinco.setOnClickListener { funcionExpresion("5", true) }
-        btSeis.setOnClickListener { funcionExpresion("6", true) }
-        btSiete.setOnClickListener { funcionExpresion("7", true) }
-        btOcho.setOnClickListener { funcionExpresion("8", true) }
-        btNueve.setOnClickListener { funcionExpresion("9", true) }
-        btCero.setOnClickListener { funcionExpresion("0", true) }
-        btPunto.setOnClickListener { funcionExpresion(".", true) }
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ) {
 
-        btSuma.setOnClickListener { funcionExpresion("+", false) }
-        btResta.setOnClickListener { funcionExpresion("-", false) }
-        btMultiplicar.setOnClickListener { funcionExpresion("*", false) }
-        btDividir.setOnClickListener { funcionExpresion("/", false) }
-        btParentesisIZQ.setOnClickListener { funcionExpresion("(", false) }
-        btParentesisDE.setOnClickListener { funcionExpresion(")", false) }
+            btUno.setOnClickListener { funcionExpresion("1", true) }
+            btDos.setOnClickListener { funcionExpresion("2", true) }
+            btTres.setOnClickListener { funcionExpresion("3", true) }
+            btCuatro.setOnClickListener { funcionExpresion("4", true) }
+            btCinco.setOnClickListener { funcionExpresion("5", true) }
+            btSeis.setOnClickListener { funcionExpresion("6", true) }
+            btSiete.setOnClickListener { funcionExpresion("7", true) }
+            btOcho.setOnClickListener { funcionExpresion("8", true) }
+            btNueve.setOnClickListener { funcionExpresion("9", true) }
+            btCero.setOnClickListener { funcionExpresion("0", true) }
+            btPunto.setOnClickListener { funcionExpresion(".", true) }
 
-        var memoria=0;
+            btSuma.setOnClickListener { funcionExpresion("+", false) }
+            btResta.setOnClickListener { funcionExpresion("-", false) }
+            btMultiplicar.setOnClickListener { funcionExpresion("*", false) }
+            btDividir.setOnClickListener { funcionExpresion("/", false) }
+            btParentesisIZQ.setOnClickListener { funcionExpresion("(", false) }
+            btParentesisDE.setOnClickListener { funcionExpresion(")", false) }
 
-        btC.setOnClickListener {
-            tvExpresion.text = ""
-            tvResultado.text = ""
-        }
+            var memoria=0;
 
-        btBorrar.setOnClickListener {
-            val string = tvExpresion.text.toString()
-            if(string.isNotEmpty()){
-                tvExpresion.text = string.substring(0,string.length-1)
+            btC.setOnClickListener {
+                tvExpresion.text = ""
+                tvResultado.text = ""
             }
-            tvResultado.text = ""
-        }
 
-        btIgual.setOnClickListener {
-            try {
-
-                val expresion = ExpressionBuilder(tvExpresion.text.toString()).build()
-                val resultado = expresion.evaluate()
-                val longResultado = resultado.toLong()
-                if(resultado == longResultado.toDouble())
-                    tvResultado.text = longResultado.toString()
-                else
-                    tvResultado.text = resultado.toString()
-
-            }catch (e:Exception){
-                Log.d("Exception"," message : " + e.message )
+            btBorrar.setOnClickListener {
+                val string = tvExpresion.text.toString()
+                if(string.isNotEmpty()){
+                    tvExpresion.text = string.substring(0,string.length-1)
+                }
+                tvResultado.text = ""
             }
-        }
 
-        btMc.setOnClickListener {
-            memoria = 0;
-            Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
-        }
+            btIgual.setOnClickListener {
+                try {
 
-        btMr.setOnClickListener {
-            if (memoria != 0) {
-                tvExpresion.setText(memoria.toString())
-                tvResultado.setText("")
-            } else {
-                tvExpresion.setText(memoria.toString())
-                tvResultado.setText("")
+                    val expresion = ExpressionBuilder(tvExpresion.text.toString()).build()
+                    val resultado = expresion.evaluate()
+                    val longResultado = resultado.toLong()
+                    if(resultado == longResultado.toDouble())
+                        tvResultado.text = longResultado.toString()
+                    else
+                        tvResultado.text = resultado.toString()
+
+                }catch (e:Exception){
+                    Log.d("Exception"," message : " + e.message )
+                }
+            }
+
+            btMc.setOnClickListener {
+                memoria = 0;
                 Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
             }
-        }
 
-        btMas.setOnClickListener {
-            if (tvResultado.text.isNotEmpty()) {
-                memoria = memoria + tvResultado.text.toString().toInt();
-                Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
+            btMr.setOnClickListener {
+                if (memoria != 0) {
+                    tvExpresion.setText(memoria.toString())
+                    tvResultado.setText("")
+                } else {
+                    tvExpresion.setText(memoria.toString())
+                    tvResultado.setText("")
+                    Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            btMas.setOnClickListener {
+                if (tvResultado.text.isNotEmpty()) {
+                    memoria = memoria + tvResultado.text.toString().toInt();
+                    Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            btMenos.setOnClickListener {
+                if (tvResultado.text.isNotEmpty()) {
+                    memoria = memoria - tvResultado.text.toString().toInt();
+                    Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
+                }
             }
         }
+        else {
 
-        btMenos.setOnClickListener {
-            if (tvResultado.text.isNotEmpty()) {
-                memoria = memoria - tvResultado.text.toString().toInt();
-                Toast.makeText(this, "Número en memoria: " + memoria, Toast.LENGTH_SHORT).show()
+            var memoriaWeb = "";
+
+            ibtBuscar.setOnClickListener() {
+                cargarWeb()
+                memoriaWeb = urlBuscador.text.toString();
+            }
+
+            ibtRecargar.setOnClickListener() {
+                val uri: Uri
+                try {
+                    uri = constructorUri(memoriaWeb)
+                    wvWeb.loadUrl(uri.toString())
+                } catch (e: UnsupportedOperationException) {
+                    Log.d("Exception"," message : " + e.message )
+                }
             }
         }
 
     }
-
+    //Funciones calculadora
     fun funcionExpresion(texto: String, limpiar: Boolean) {
 
         if(tvResultado.text.isNotEmpty()){
@@ -112,6 +138,25 @@ class MainActivity : AppCompatActivity() {
             tvExpresion.append(tvResultado.text)
             tvExpresion.append(texto)
             tvResultado.text = ""
+        }
+    }
+    //Funciones buscador web
+    fun constructorUri(authority: String): Uri {
+        val constructor = Uri.Builder()
+        constructor.scheme("https")
+            .authority(authority)
+        return constructor.build()
+    }
+
+    fun cargarWeb() {
+        wvWeb.loadUrl("")
+        val uri: Uri
+
+        try {
+            uri = constructorUri(urlBuscador.text.toString())
+            wvWeb.loadUrl(uri.toString())
+        } catch (e: UnsupportedOperationException) {
+            Log.d("Exception"," message : " + e.message )
         }
     }
 }
