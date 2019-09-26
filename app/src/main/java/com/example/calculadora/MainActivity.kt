95d7d406e18ea7b44089d66486eddab85c776fe4
jomiferse <ljose500.jf@gmail.com>
2019-09-26 17:49:11 +0200

    V2

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -140,7 +140,7 @@ class MainActivity : AppCompatActivity() {
             tvResultado.text = ""
         }
     }
-    //Funciones buscador web
+    //Funciones buscador
     fun constructorUri(authority: String): Uri {
         val constructor = Uri.Builder()
         constructor.scheme("https")
